10ed26876bc2a71817b1495a6c285d2e752e13ba
leticiacaldeira-creator <leticia.caldeira@edu.senai.br>
2026-08-20 11:42:33 -0300

    Adiciona seção de contato e validação com JS

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("form-contato").addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+
+    if (nome === "" || email === "") {
+        alert("Por favor, preencha todos os campos.");
+        } else {
+            alert("Mensagem enviada com sucesso")
+        }
+
+});
+    
